f5ba3ffdc81dcb50f9507fac463ee033e88ba6a0
romushka <romushka1978@gmail.com>
2025-01-15 21:22:12 +0100

    Problem 20

--- code_advent_2024/tasks/task_20.py
+++ code_advent_2024/tasks/task_20.py
@@ -1,17 +1,56 @@
 """""Task 20 solver."""
 import dataclasses
+import enum
 from code_advent_2024.tasks import task
+
+_WALL_CHAR = "#"
+_START_POSITION_CHAR = "S"
+_END_POSITION_CHAR = "E"
+
+
+@dataclasses.dataclass(frozen=True)
+class Coordinates:
+    """Position in the map."""
+    row: int = 0
+    col: int = 0
+
+
+@dataclasses.dataclass(frozen=True)
+class CoordinatesDelta:
+    """Position in the map."""
+    d_row: int = 0
+    d_col: int = 0
+
+
+class Direction(enum.IntEnum):
+    """Directions in the map."""
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+
+_DIRECTION_DELTA = {
+    Direction.NORTH: CoordinatesDelta(d_row=-1, d_col=0),
+    Direction.EAST: CoordinatesDelta(d_row=0, d_col=1),
+    Direction.SOUTH: CoordinatesDelta(d_row=1, d_col=0),
+    Direction.WEST: CoordinatesDelta(d_row=0, d_col=-1)
+}
 
 
 @dataclasses.dataclass(frozen=True)
 class TaskInput(task.TaskInput):
     """Represents task input."""
+    start: Coordinates = None
+    end: Coordinates = None
+    walls: frozenset[Coordinates] = frozenset()
+    min_number_of_saved_picoseconds: int = 100
 
 
 @dataclasses.dataclass(frozen=True, )
 class TaskSolution(task.TaskSolution):
     """Represents task solution."""
-    program_output: str = ""
+    number_of_cheats: int = 0
 
 
 def get_input_from_file(file_name: str) -> TaskInput:
@@ -32,17 +71,113 @@ def get_input_from_string(input_string: str) -> TaskInput:
     Args:
         input_string: Input data as a string.
     """
+    walls = []
+    start = None
+    end = None
+    for row_idx, line in enumerate(input_string.split("\n")):
+        for col_idx, char in enumerate(line):
+            coord = Coordinates(row=row_idx, col=col_idx)
+            if char == _WALL_CHAR:
+                walls.append(coord)
+            elif char == _START_POSITION_CHAR:
+                start = coord
+            elif char == _END_POSITION_CHAR:
+                end = coord
 
-    return TaskInput()
+    return TaskInput(start=start, end=end, walls=frozenset(walls))
+
+
+def _build_trail(
+    walls: frozenset[Coordinates],
+    start: Coordinates,
+    end: Coordinates
+) -> dict[Coordinates, int]:
+    trail = {}
+    current_position = start
+    prev_position = None
+    number_of_steps = 0
+    trail[current_position] = number_of_steps
+    while current_position != end:
+        current_position, prev_position = _get_next_position(
+            walls=walls,
+            current_position=current_position,
+            prev_position=prev_position
+        )
+        number_of_steps += 1
+        trail[current_position] = number_of_steps
+
+    return trail
+
+
+def _get_next_position(
+    walls: frozenset[Coordinates],
+    current_position: Coordinates,
+    prev_position: Coordinates
+) -> Coordinates:
+    for direction in Direction:
+        next_position = Coordinates(
+            row=current_position.row + _DIRECTION_DELTA[direction].d_row,
+            col=current_position.col + _DIRECTION_DELTA[direction].d_col
+        )
+        if next_position not in walls and next_position != prev_position:
+            return next_position, current_position
+
+    return current_position, prev_position
+
+
+def _calc_cheats(
+    walls: frozenset[Coordinates],
+    trail: dict[Coordinates, int],
+    min_number_of_saved_picoseconds: int
+) -> int:
+    saved_time_to_number_of_cheats = {}
+    for position, steps in trail.items():
+        for direction in Direction:
+            next_coord = Coordinates(
+                row=position.row + _DIRECTION_DELTA[direction].d_row,
+                col=position.col + _DIRECTION_DELTA[direction].d_col
+            )
+            next_next_coord = Coordinates(
+                row=next_coord.row + _DIRECTION_DELTA[direction].d_row,
+                col=next_coord.col + _DIRECTION_DELTA[direction].d_col
+            )
+            if (
+                next_coord in walls
+                and next_next_coord in trail
+                and trail[next_next_coord] > steps
+            ):
+                saved_picoseconds = trail[next_next_coord] - steps - 2
+                saved_time_to_number_of_cheats.setdefault(
+                    saved_picoseconds, 0
+                )
+                saved_time_to_number_of_cheats[saved_picoseconds] += 1
+
+    return sum(
+        number_of_cheats
+        for saved_time, number_of_cheats in saved_time_to_number_of_cheats.items()
+        if saved_time >= min_number_of_saved_picoseconds
+    )
 
 
 def solve_part1(task_input: TaskInput) -> TaskSolution:
     "Solve the first part of the task."
+    trail = _build_trail(
+        walls=task_input.walls,
+        start=task_input.start,
+        end=task_input.end
+    )
 
-    return TaskSolution(program_output="")
+    number_of_cheats = _calc_cheats(
+        walls=task_input.walls,
+        trail=trail,
+        min_number_of_saved_picoseconds=task_input.min_number_of_saved_picoseconds
+    )
+
+    return TaskSolution(number_of_cheats=number_of_cheats)
 
 
 def solve_part2(task_input: TaskInput) -> TaskSolution:
     """Solve the second part of the task."""
+    number_of_cheats = 0
 
-    return TaskSolution(program_output="")
+    return TaskSolution(number_of_cheats=number_of_cheats)
--- tests/test_task_20.py
+++ tests/test_task_20.py
@@ -1,5 +1,7 @@
 """Task 20 test cases."""
 import unittest
+import dataclasses
+
 from code_advent_2024.tasks import task_20
 
 _INPUT = """
@@ -27,13 +29,16 @@ class TaskTest(unittest.TestCase):
     def setUp(self):
         super().setUp()
         self.task_input = task_20.get_input_from_string(_INPUT)
+        self.task_input = dataclasses.replace(
+            self.task_input, min_number_of_saved_picoseconds=12)
 
     def test_solve_part1(self):
         """Tests solve_part1 method."""
         actual_value = task_20.solve_part1(self.task_input)
 
         expected_value = task_20.TaskSolution(
-            program_output="4,6,3,5,6,3,5,2,1,0")
+            number_of_cheats=8
+        )
         self.assertEqual(expected_value, actual_value)
 
     @unittest.skip("Not implemented yet")
@@ -41,7 +46,9 @@ class TaskTest(unittest.TestCase):
         """Tests solve_part2 method."""
         actual_value = task_20.solve_part2(self.task_input)
 
-        expected_value = task_20.TaskSolution(program_output=None)
+        expected_value = task_20.TaskSolution(
+            number_of_cheats=8
+        )
         self.assertEqual(expected_value, actual_value)
 
 
